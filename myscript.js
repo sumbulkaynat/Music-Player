8976c2b343991a98ca7e55e3e95f8c670c48b445
Sumbul Kaynat <sumbulkaynat@gmail.com>
2020-06-24 18:00:33 +0530

    added one song in footer

--- myscript.js
+++ myscript.js
@@ -1,3 +1,54 @@
+var song = new Audio;
+var muted = false;
+var vol = 1;
+song.type = 'audio/mpeg';
+song.src = 'audio/song1.mp3';//Audio file source url
+
+function skip(time) {
+   if (time == 'back') {
+     song.currentTime = (song.currentTime - 5);
+   } else if (time == 'fwd') {
+     song.currentTime = (song.currentTime + 5);
+   }
+}
+function playpause() {
+  if (!song.paused) {
+    song.pause();
+  } else {
+    song.play();
+  }
+}
+function stop() {
+  song.pause();
+  song.currentTime = 0;
+  document.getElementById('seek').value = 0;
+}
+function setPos(pos) {
+  song.currentTime = pos;
+}
+function mute() {
+  if (muted) {
+    song.volume = vol;
+    muted = false;
+    document.getElementById('mute').innerHTML = '<i class="fa fa-volume-up"></i>';
+  } else {
+    song.volume = 0;
+    muted = true;
+    document.getElementById('mute').innerHTML = '<i class="fa fa-volume-off"></i>';
+  }
+}
+function setVolume(volume) {
+  song.volume = volume;
+  vol = volume;
+}
+song.addEventListener('timeupdate',function() {
+  curtime = parseInt(song.currentTime,10);
+  document.getElementById('seek').max = song.duration;
+  document.getElementById('seek').value = curtime;
+})
+
+/*================================queue==============*/
+
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
@@ -18,63 +69,5 @@ window.onclick = function(event) {
   }
 }
 
-/*================================================*/
-/*
-  2ND DAY OF MY PERSONAL CHALLENGE
-  START TIME: 5:25 PM
-  END TIME: 7:03 PM
-  BASED ON:
-  https://www.materialup.com/posts/daily-ui-009-music-player
-*/
-$(document).ready(function() {
-  $('#range-val').on("change mousemove", function() {
-    //Lets change it in time mode
-    var original_time = $(this).val();
-    var new_time = formatSeconds(original_time);
-    $("#time p").text(new_time);
-  });
-  /*
-  ANOTHER GREAT WORK BY SOMEONE...
-  http://jsfiddle.net/CYSeY/4/
-  i just modify it
-  */
-  var r = document.getElementById('range-val');
-  var max = r.getAttribute('max');
-  var min = r.getAttribute('min');
-  var w = r.clientWidth;
-  //w += r.offsetLeft;
-  var isDragging = false;
 
-  var moveTip = (function(e) {
-    if (isDragging) {
-      var posPerc = (r.value / max) * 100;
-      var pixPos = (posPerc / 100) * w - 40;
-      /* pixPos += r.offsetLeft;*/
 
-      document.getElementById('tip').style.display = 'block';
-      document.getElementById('tip').style.left = pixPos + 'px';
-    }
-  });
-
-  $("#range-val").mousemove(function() {
-    isDragging = true;
-    var range_val = document.getElementById("range-val").value;
-    document.getElementById('tip').innerHTML = formatSeconds(range_val);
-    r.addEventListener('mousemove', moveTip, false);
-  });
-
-  $("body").mouseup(function(e) {
-    isDragging = false;
-    r.removeEventListener('mousemove', moveTip);
-    document.getElementById('tip').style.display = 'none';
-  });
-});
-/*
-FUNCTIONS (I just modify it)
-http://stackoverflow.com/a/17781037
-*/
-function formatSeconds(seconds) {
-  var date = new Date(1970, 0, 1);
-  date.setSeconds(seconds);
-  return date.toTimeString().replace(/.*(\d{2}:\d{2}).*/, "$1");
-}
